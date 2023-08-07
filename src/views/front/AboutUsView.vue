6f050ea2e6f5c82daa2f40f6bdfb02e60982794f
YaWenWu1008 <belov3318@gmail.com>
2023-08-07 20:27:57 +0800

    aboutus 版面

--- src/views/front/AboutUsView.vue
+++ src/views/front/AboutUsView.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>about us</h1>
+  <div class="aboutImg">Our Story</div>
+  <v-container>
+    <v-row>
+      <v-col class="aboutCol">
+        <p>創業之路，有時會像攀登一座高山，艱難陡峭，充滿未知和風險。但就像石頭中蘊含著珍貴的寶石，酒桃不論遇到多少困難和挑戰，都會因為它們變得更加堅韌和成熟。</p>
+        <p>酒桃取名諧音「石頭」，正是期許能像石頭在歷經長時間的歲月風雨後，變得更加堅韌和耐久，創業者也需要在時間的考驗中，不斷磨練自己，將打造清酒界的「酒桃」烏托邦。</p>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>來店須知</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis fugiat, magni, officiis dicta quaerat,
+          dolorum numquam asperiores inventore repellat libero reiciendis nam hic voluptate. Aut aspernatur similique
+          culpa saepe exercitationem.</p>
+      </v-col>
+      <v-col><img src="../../assets/cheers.jpg"></v-col>
+    </v-row>
+  </v-container>
 </template>
