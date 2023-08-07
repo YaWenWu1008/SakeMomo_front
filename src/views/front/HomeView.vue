9fc0e848ba34e1e2f5769e78b028613932e4f68f
YaWenWu1008 <belov3318@gmail.com>
2023-08-07 19:45:10 +0800

    homeview informatiom

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -33,10 +33,36 @@
     </v-row>
   </v-container>
 
+  <h1 class="text-center mb-3">相片集</h1>
   <v-carousel hide-delimiters class="homeCarousel">
-    <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" contain>
-    </v-carousel-item>
+    <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" cover></v-carousel-item>
   </v-carousel>
+
+  <h1 class="text-center mb-3">INFORMATION</h1>
+  <v-container class="homeInformation2">
+    <v-row class="homeInformation2Row">
+      <v-col class="homeInformation2Col">
+        <span>address</span>
+        <span>台北市大安區光復南路456巷6號</span>
+      </v-col>
+      <v-col class="homeInformation2Col">
+        <span>TEL</span>
+        <span>02 2703 7507</span>
+      </v-col>
+      <v-col class="homeInformation2Col">
+        <span>營業時間</span>
+        <div class="d-flex flex-wrap ">
+          <span>FRI-SAT : 18:00 ~ 1:00</span>
+          <span>SUN-THU : 18:00 ~ 00:00</span>
+        </div>
+      </v-col>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.9583868108703!2d121.55445587600065!3d25.03548623823055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ab22532c8b1b%3A0x5456aa82472b085!2z6YWS5qGDU2FrZSBtb21v!5e0!3m2!1szh-TW!2stw!4v1691407659694!5m2!1szh-TW!2stw"
+        width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </v-row>
+
+  </v-container>
 </template>
 
 <script>
@@ -57,7 +83,10 @@ export default {
           src: 'https://lh3.googleusercontent.com/p/AF1QipMq5PAP38N8LkbwAvcQPywXQjJdoGnOLS4rylJG=s680-w680-h510'
         },
         {
-          src: 'https://lh3.googleusercontent.com/p/AF1QipPlIj_YBQ2N0_8rlVFcJYq_WAIuZn0PtyCFDwMd=s680-w680-h510'
+          src: 'https://lh3.googleusercontent.com/p/AF1QipNNPyFF3T5UzrsnqcVyt9jP1_QQgg0T1PSlh5JH=s680-w680-h510'
+        },
+        {
+          src: 'https://lh3.googleusercontent.com/p/AF1QipPuw1P0XigDQmxM1bXgN0uppwrinq6itgSCLOQx=s680-w680-h510'
         }
       ]
     }
